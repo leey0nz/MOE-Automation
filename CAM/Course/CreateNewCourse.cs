d617ca3cb041e588b21e5738c71a7e7aca7edf43
tuan trinh <tuan.trinh@orientsoftware.com>
2020-05-21 17:51:21 +0700

    Upload Image and Click Save btn

--- CAM/Course/CreateNewCourse.cs
+++ CAM/Course/CreateNewCourse.cs
@@ -1,4 +1,5 @@
-﻿using Login.Common.Login;
+﻿using AutoItX3Lib;
+using Login.Common.Login;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -22,12 +23,10 @@ namespace CAM
             //driver.Manage().Window.Maximize();
             verificationErrors = new StringBuilder();
             driver.Navigate().GoToUrl("https://www.development.opal2.conexus.net/app/");
-
+            AutoItX3 autoIt = new AutoItX3();
+            autoIt.WinActivate("Open"); 
             LoginPage.Login(driver);
-
-
-
-            Thread.Sleep(2000);
+            Thread.Sleep(3000);
 
             AddCourse addingCourse = new AddCourse(driver);
             Thread.Sleep(5000);
@@ -37,8 +36,14 @@ namespace CAM
             Thread.Sleep(4000);
             addingCourse.ClickButton._ClickButton("//button[@class='k-button-icontext k-button k-primary']");
             addingCourse.ClickButton._ClickButton("//li[@class='k-item ng-star-inserted k-state-focused']");
+            Thread.Sleep(2000);
             addingCourse.ClickButton._ClickButton("//opal-file-uploader[@class='basic-info-tab__thumbnail-uploader ng-untouched ng-pristine ng-valid ng-star-inserted']");
-            addingCourse.EnterTextBoxByElement.EnterTexts("//opal-file-uploader[@class='basic-info-tab__thumbnail-uploader ng-untouched ng-pristine ng-valid ng-star-inserted']", "C:\\Users\tuan.trinh\\Downloads\\i.jpg");
+            autoIt.Send("C:\\Users\\tuan.trinh\\Downloads\\i.jpg");
+            Thread.Sleep(2000);
+            autoIt.Send("{ENTER}");
+            Thread.Sleep(2000);
+            addingCourse.ClickButton._ClickButton("//button[@class='k-button ng-star-inserted']");
+
             //addingCategoriesPageObject.ClickButton._ClickButton("//div[@class='group-button on-top']//button[@class='btn btn-outline-secondary reset'][contains(text(),'Add')]");
             //addingCategoriesPageObject.EnterTextBox.EnterTexts("//input[@placeholder='Name']", "Testing 1");
             //addingCategoriesPageObject.EnterTextBox.EnterTexts("//input[@placeholder='Description']", "Testing 1");
